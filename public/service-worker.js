ee86d365fdcd18f90a52d267aecde0c58f48869a
Amitabh Kumar <amitabh.kumar2@gmail.com>
2020-03-16 10:28:22 -0400

    service-worker with files to cache

---
+++ public/service-worker.js
@@ -0,0 +1,14 @@
+
+var FILES_TO_CACHE = [
+    '/',
+    '/styles.css',
+    '/index.js',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
+    '/manifest.json',
+    '/db.js'  
+  ];
+  
+  const CACHE_NAME = "static-cache-v2";
+  const DATA_CACHE_NAME = "data-cache-v1";
+  
