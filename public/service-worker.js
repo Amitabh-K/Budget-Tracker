c445dccc5f45f4be713028a4b2a149ee0c624df2
Amitabh Kumar <amitabh.kumar2@gmail.com>
2020-03-16 10:30:03 -0400

    install

--- public/service-worker.js
+++ public/service-worker.js
@@ -11,4 +11,15 @@ var FILES_TO_CACHE = [
   
   const CACHE_NAME = "static-cache-v2";
   const DATA_CACHE_NAME = "data-cache-v1";
+
+  
+// install
+self.addEventListener("install", function(evt) {
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then(cache => {
+        return cache.addAll(FILES_TO_CACHE);
+      })
+    );  
+    self.skipWaiting();
+  });
   
